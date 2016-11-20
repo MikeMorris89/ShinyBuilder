368b92ebe2b7f90d1ba2ed46fc191eba9b2339f3
MikeMorris89 <Mike.Morris89@gmail.com>
2016-11-20 18:28:38 -0500

    Revert "permiss"
    This reverts commit 63bc52d2a957d3f510ec72161e4e11c5b0e90964.

--- R/reinstall_from_git.R
+++
@@ -1,6 +0,0 @@
-remove.packages('ShinyBuilder')
-
-devtools::install_github("mikemorris89/shinyMCE")
-devtools::install_github("shinyGoogleCharts", "mul118")
-devtools::install_github("mikemorris89/shinyGridster")
-devtools::install_github("mikemorris89/ShinyBuilder")
